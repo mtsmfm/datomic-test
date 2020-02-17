0af119d4b96149386fb49c33946ee5526a83de81
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2020-02-17 12:58:02 +0000

    wip

--- backend/project.clj
+++ backend/project.clj
@@ -3,7 +3,13 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"], [com.datomic/client-pro "0.9.43"]]
+  :dependencies [
+    [org.clojure/clojure "1.10.0"]
+    [com.datomic/client-pro "0.9.43"]
+    [com.walmartlabs/lacinia "0.36.0"]
+    [com.walmartlabs/lacinia-pedestal "0.13.0"]
+    [org.slf4j/slf4j-simple "1.7.30"]]
+  :plugins [[lein-cljfmt "0.6.6"]]
   :main ^:skip-aot backend.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- backend/src/backend/core.clj
+++ backend/src/backend/core.clj
@@ -1,7 +1,25 @@
 (ns backend.core
-  (:gen-class))
+  (:require [io.pedestal.http :as http]
+            [com.walmartlabs.lacinia.pedestal :as lacinia]
+            [com.walmartlabs.lacinia.schema :as schema]))
+
+(def schema (schema/compile
+             {:objects
+              {:todo
+               {:fields {:id {:type ID}
+                         :title {:type String}
+                         :archived {:type Boolean}}}}
+
+              :queries {:todos
+                        {:type 'String
+                         :resolve (constantly "world")}}}))
+
+(def service (lacinia/service-map schema))
+
+(defonce runnable-service (http/create-server service))
 
 (defn -main
-  "I don't do a whole lot ... yet."
+  "The entry-point for 'lein run'"
   [& args]
-  (println "Hello, World!"))
+  (println "\nCreating your server...")
+  (http/start runnable-service))
